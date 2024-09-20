cf6859e19bd1517def6e9febb108bb0678115fc3
Daniel Smith <ds1239@hotmail.com>
2024-09-20 10:21:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ class JewishCalendar {
         this.log = log;
 
         // this.today = new Date();
-        this.today = new Date("2024-10-03T20:57:00-04:00");
+        this.today = new Date("2024-10-04T20:57:00-04:00");
         this.lat = parseFloat(config.latitude);
         this.long = parseFloat(config.longitude);
         this.name = config.name;
@@ -192,7 +192,7 @@ class JewishCalendar {
 
     updateLoop() {
         // this.today = new Date();
-        this.today = new Date("2024-10-03T20:57:00-04:00");
+        this.today = new Date("2024-10-04T20:57:00-04:00");
         
         this.updateJewishDay();
         setTimeout(this.updateLoop.bind(this), 10000);
