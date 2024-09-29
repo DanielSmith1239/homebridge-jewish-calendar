34c8221a0e1d98a8342c20b6ddb4bc19fc2032ad
Daniel Smith <ds1239@hotmail.com>
2024-09-29 11:50:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -145,6 +145,10 @@ class JewishCalendar {
         const firstCandleLightingDate = new Date(candles["date"]);
         const memo = !(candles["memo"] ?? "").includes("II") ? candles["memo"] : "shab";
 
+        this.log.info(candles);
+        this.log.info("first:");
+        this.log.info(firstCandleLightingDate);
+
         if (this.isAfterToday(firstCandleLightingDate)) {
             return "";
         }
